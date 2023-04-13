ee3cab26b867fbfdbed040e1cecbb276a9f741db
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2023-04-13 21:17:19 +0200

    fix

--- simclr/modules/custom_infonce.py
+++ simclr/modules/custom_infonce.py
@@ -40,13 +40,13 @@ class Custom_InfoNCE(nn.Module):
             total_loss_value = torch.mean(- pos + neg)
         elif self.bound:
             if self.subsample:
-                keep=random.shuffle(list(np.range(anchor_rec.shape[0])))
-                sim11=sim11[keep,:,:]
-                sim12=sim12[keep,:,:]
-                sim22=sim22[keep,:,:]
-            num = - torch.mean(torch.log(sim12[..., range(d), range(d)]),dim=-1)
+                keep=random.shuffle(list(np.range(anchor_rec.shape[1])))
+                sim11=sim11[:,keep,:]
+                sim12=sim12[:,keep,:]
+                sim22=sim22[:,keep,:]
+            num = - torch.mean(torch.log(sim12[..., range(d), range(d)]),dim=1)
             deno = torch.cat([sim12, sim11], dim=-1)
-            deno = torch.log(torch.sum(torch.mean(deno,dim=-1),dim=1))
+            deno = torch.log(torch.sum(torch.mean(deno,dim=1),dim=1))
             total_loss_value = torch.mean(num + deno)
         else:
             # diagonal - targets where the values should be the highest
@@ -62,10 +62,10 @@ class Custom_InfoNCE(nn.Module):
                 total_loss_value += torch.mean(- pos + neg)
             elif self.bound:
                 if self.subsample:
-                    sim12=sim12[keep,:,:]
-                num = - torch.mean(torch.log(sim12[..., range(d), range(d)]),dim=-1)
+                    sim12=sim12[:,keep,:]
+                num = - torch.mean(torch.log(sim12[..., range(d), range(d)]),dim=1)
                 deno = torch.cat([sim12, sim22], dim=-1)
-                deno = torch.log(torch.sum(torch.mean(deno,dim=-1),dim=1))
+                deno = torch.log(torch.sum(torch.mean(deno,dim=1),dim=1))
                 total_loss_value += torch.mean(num + deno)
             else:
                 # creating matrix with all similarities
@@ -82,10 +82,10 @@ def custom_similarity(p_z_zrec,p_zpos_zrecpos,bound,subsample):
     p_zpos_zrecpos = (p_zpos_zrecpos+1e-8) # / p_zpos
     if not bound:
         if subsample: 
-            keep=random.shuffle(list(np.range(p_z_zrec.shape[0])))
-            p_z_zrec = p_z_zrec[keep,:,:]
-            p_zpos_zrecpos = p_zpos_zrecpos[keep,:,:]
-        else: return torch.log(torch.sum(p_z_zrec*p_zpos_zrecpos,dim=-1))  # log because cross entropy adds an exp
+            keep=random.shuffle(list(np.range(p_z_zrec.shape[1])))
+            p_z_zrec = p_z_zrec[:,keep,:]
+            p_zpos_zrecpos = p_zpos_zrecpos[:,keep,:]
+        else: return torch.log(torch.sum(p_z_zrec*p_zpos_zrecpos,dim=1))  # log because cross entropy adds an exp
     else: return p_z_zrec*p_zpos_zrecpos
 
 
