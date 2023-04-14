26a2b05ff94e13582021f8e7ac0dec2846b56d1f
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2023-04-14 12:13:49 +0200

    print

--- simclr/modules/custom_infonce.py
+++ simclr/modules/custom_infonce.py
@@ -34,10 +34,8 @@ class Custom_InfoNCE(nn.Module):
             sim11 = sim11.flatten()[1:].view(sim11.shape[0]-1, sim11.shape[0]+1)[:,:-1].reshape(sim11.shape[0], sim11.shape[0]-1)
             sim22 = sim22.flatten()[1:].view(sim22.shape[0]-1, sim22.shape[0]+1)[:,:-1].reshape(sim22.shape[0], sim22.shape[0]-1)
         else:
-            list_index=[i*(sim11.shape[0]+1)+j*sim11.shape[0] for i in range(sim11.shape[0]) for j in range(sim11.shape[1])]
-            print(list_index)
-            mask = [0 if index in list_index else 1 for index in range(sim11.shape[0]*sim11.shape[1]*sim11.shape[2])]
-            print(list_index,mask)
+            mask = sim11.flatten()[idx for idx in list(range(sim11.shape[0]*sim11.shape[1]*sim11.shape[2])) if (idx-((idx//(sim11.shape[0]*sim11.shape[-1]))*(sim11.shape[0]*sim11.shape[-1])))%sim11.shape[0] != ((idx-((idx//(sim11.shape[0]*sim11.shape[-1]))*(sim11.shape[0]*sim11.shape[-1])))//sim11.shape[0]) else -1].remove(-1)
+            print(mask)
             sim11 = torch.transpose(torch.transpose(sim11,0,1).flatten()[1:].view(sim11.shape[1],sim11.shape[0]-1, sim11.shape[0]+1, )[:,:,:-1].reshape(sim11.shape[1],sim11.shape[0], sim11.shape[0]-1,),0,1)
             sim22 = torch.transpose(torch.transpose(sim22,0,1).flatten()[1:].view(sim22.shape[1],sim22.shape[0]-1, sim22.shape[0]+1,)[:,:,:-1].reshape(sim22.shape[1],sim22.shape[0], sim22.shape[0]-1,),0,1)
 
