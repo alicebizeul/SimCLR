2aac2e6aa69af49b0e36f531d13f3eb54d3a0d14
Alice Elsa Marie Bizeul <alice.bizeul@inf.ethz.ch>
2023-04-16 11:04:16 +0200

    keep

--- simclr/modules/custom_infonce.py
+++ simclr/modules/custom_infonce.py
@@ -41,7 +41,8 @@ class Custom_InfoNCE(nn.Module):
         #     total_loss_value = torch.mean(- pos + neg)
         if self.bound:
             if self.subsample:
-                keep=random.shuffle(list(np.arange(int(anchor_rec.shape[1]))))
+                random.shuffle(list(np.arange(int(anchor_rec.shape[1]))))
+                keep = keep[:int(len(keep)/2)]
                 print(keep) #[:int(int(anchor_rec.shape[1])/2)]
                 sim11=sim11[:,keep,:]
                 sim12=sim12[:,keep,:]
